41ccde46a796aea6729f966bec5c0a2493c9c8a8
murilopbc <murilopoltronieri3@gmail.com>
2025-05-07 16:23:07 -0300

    criação da tabela de servicos

---
+++ database/migrations/2025_05_07_161125_create_servicos_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('servicos', function (Blueprint $table) {
+            $table->id();
+            $table->string('nome_servico');
+            $table->string('descricao');
+            $table->decimal('valor', 8, 2);
+            $table->decimal('valor_fim_semana', 8, 2);
+            $table->integer('desconto');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('servicos');
+    }
+};
